70dd7b1b91fd1b416c9a112bddd15b82229cb0ae
shermai2022 <sheralirashitov43@gmail.com>
2022-12-26 00:55:17 +0500

    Lab 4 is done

---
+++ Lab4/lab4_4.py
@@ -0,0 +1,15 @@
+A = {'name': "Lich King",
+ 'Status': "King of Death",
+ 'race':"Undead"}
+
+def slavar(dict, key):
+    
+    b = dict[key]
+    print(b)
+    
+    return
+    
+    
+    
+    
+slavar(A, 'name')
