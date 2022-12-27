cc380300c7df67b01c27a548fe8ba3f372bbd215
shermai2022 <sheralirashitov43@gmail.com>
2022-12-27 22:00:01 +0500

    I did it

---
+++ Lab4/Figures/The_Square.py
@@ -0,0 +1,12 @@
+def square_perimeter(a = 5):
+    peri = a*4
+    return peri
+    
+    
+def square_area(a = 5):
+    area = a**2
+    return area
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
